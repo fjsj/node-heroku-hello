973d8b39e144857a3c1e7e8bce928c0b8cf6a1ab
Flávio Juvenal <flaviojuvenal@gmail.com>
2011-09-07 00:26:11 -0300

    README

---
+++ README.md
@@ -0,0 +1,2 @@
+# node-heroku-hello
+A ready to deploy on Heroku Hello World Node.js app. Just push with `git push heroku master`.
--- web.js
+++ web.js
@@ -1,13 +1,10 @@
-// Load the http module to create an http server.
 var http = require('http');
 
-// Configure our HTTP server to respond with Hello World to all requests.
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
   response.end("Hello World\n");
 });
 
-var port = process.env.PORT || 3000; //get port from heroku
+var port = process.env.PORT || 3000;
 server.listen(port);
-// Put a friendly message on the terminal
 console.log("Server running at http://127.0.0.1/ on port " + port);
